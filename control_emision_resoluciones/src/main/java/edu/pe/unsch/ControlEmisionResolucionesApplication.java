9813cb6c126197364181ab33f80ad5b40c488e84
deybersmithayalabellido <50890703+deybersmithayalabellido@users.noreply.github.com>
2019-06-07 03:45:07 -0500

    Entregable 1

---
+++ control_emision_resoluciones/src/main/java/edu/pe/unsch/ControlEmisionResolucionesApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.unsch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlEmisionResolucionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlEmisionResolucionesApplication.class, args);
+	}
+
+}
